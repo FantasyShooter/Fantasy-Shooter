4ee68b7df683899bd688a4b109606b24a2b337c6
tmrcwwong <tmrcwwong@msn.com>
2014-04-08 15:44:24 +0800

    asdasdasdasd

---
+++ FantasyShooter/src/com/fshoot/framepage/BattlePage.java
@@ -0,0 +1,14 @@
+package com.fshoot.framepage;
+
+import android.app.Activity;
+
+public class BattlePage implements FramePage{
+
+	private Activity activity;
+	
+	@Override
+	public void show(Activity activity) {
+
+	}
+
+}
