f329dfa0019218862884097c745cba5e7f088b42
kenneth2008 <kannif2008@gmail.com>
2014-04-08 18:01:41 +0800

    teaasdasdasda

--- FantasyShooter/src/com/example/fantasyshooter/MainActivity.java
+++ FantasyShooter/src/com/example/fantasyshooter/MainActivity.java
@@ -10,13 +10,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Create the main menu here
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.test
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.test
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return true;
+//	}
 
 }
