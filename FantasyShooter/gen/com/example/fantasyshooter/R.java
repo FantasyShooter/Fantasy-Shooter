a9d31168672a91dc8c8384b99144cb07e10a4e84
tmrcwwong <tmrcwwong@msn.com>
2014-04-15 12:24:04 +0800

    start page, some background photo

--- FantasyShooter/gen/com/example/fantasyshooter/R.java
+++ FantasyShooter/gen/com/example/fantasyshooter/R.java
@@ -25,29 +25,32 @@ public final class R {
         public static final int boss=0x7f020001;
         public static final int fhero=0x7f020002;
         public static final int fire_graphic=0x7f020003;
-        public static final int gun=0x7f020004;
-        public static final int hero_new=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int icon_coin=0x7f020007;
-        public static final int mhero=0x7f020008;
-        public static final int middle=0x7f020009;
-        public static final int reload=0x7f02000a;
-        public static final int sky=0x7f02000b;
-        public static final int small=0x7f02000c;
-        public static final int town=0x7f02000d;
+        public static final int grass=0x7f020004;
+        public static final int gun=0x7f020005;
+        public static final int hero_new=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int icon_coin=0x7f020008;
+        public static final int mhero=0x7f020009;
+        public static final int middle=0x7f02000a;
+        public static final int reload=0x7f02000b;
+        public static final int sky=0x7f02000c;
+        public static final int small=0x7f02000d;
+        public static final int town=0x7f02000e;
     }
     public static final class id {
         public static final int ImageView1=0x7f080008;
         public static final int abutton=0x7f080001;
-        public static final int action_settings=0x7f08000b;
+        public static final int action_settings=0x7f08000d;
         public static final int atextview=0x7f080000;
         public static final int battleRoot=0x7f080004;
         public static final int button1=0x7f080002;
+        public static final int button2=0x7f08000c;
         public static final int content_frame=0x7f080003;
         public static final int imageButton1=0x7f08000a;
         public static final int imageButton2=0x7f080009;
         public static final int leftRoot=0x7f080006;
         public static final int rightRoot=0x7f080007;
+        public static final int textView1=0x7f08000b;
         public static final int topRoot=0x7f080005;
     }
     public static final class layout {
@@ -55,6 +58,7 @@ public final class R {
         public static final int activity_main=0x7f030001;
         public static final int frame_battle=0x7f030002;
         public static final int frame_menu=0x7f030003;
+        public static final int frame_start=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- FantasyShooter/src/com/fshoot/framepage/StartPage.java
+++ FantasyShooter/src/com/fshoot/framepage/StartPage.java
@@ -3,41 +3,42 @@ package com.fshoot.framepage;
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.FrameLayout;
-import com.fshoot.framepage.BattlePage;
+import android.widget.Button;
+
 import com.example.fantasyshooter.R;
 
-public class MenuPage implements FramePage{
+public class StartPage implements FramePage{
 	
-	private Activity activity;
 	@Override
 	public void show(Activity activity) {
 		// 1. GUI
-		this.activity = activity;
 		FrameLayout frame = (FrameLayout) (activity.findViewById(R.id.content_frame));
 		// Remove the old one
 		frame.removeAllViewsInLayout();
 		// Show a new screen
-		LayoutInflater.from(activity).inflate(R.layout.frame_menu, frame,true);
-		//battle button
-		ImageButton battle = (ImageButton) (activity.findViewById(R.id.imageButton1));
-		battle.setOnClickListener(new View.OnClickListener() {
+		LayoutInflater.from(activity).inflate(R.layout.frame_start, frame,true);
+		
+		// 2. setup event listener
+		
+		// 3. event start when created
+		//create data base
+		//start button
+		Button start = (Button) (activity.findViewById(R.id.button1));
+		start.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				new BattlePage().show((Activity)v.getContext());
             }
 		});
-		//player button
-		ImageButton player = (ImageButton) (activity.findViewById(R.id.imageButton2));
-		player.setOnClickListener(new View.OnClickListener() {
+		//score button
+		Button score = (Button) (activity.findViewById(R.id.button2));
+		score.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				//new BattlePage.show(this);
+				//new ScorePage().show((Activity)v.getContext());
             }
 		});
-
-
 		
 	}
 
