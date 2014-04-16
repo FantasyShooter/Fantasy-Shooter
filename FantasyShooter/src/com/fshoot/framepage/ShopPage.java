5c70c5dd564edaa70e9c17c1189e993c70c219e7
tmrcwwong <tmrcwwong@msn.com>
2014-04-16 13:35:12 +0800

    shop page

--- FantasyShooter/src/com/fshoot/framepage/ShopPage.java
+++ FantasyShooter/src/com/fshoot/framepage/ShopPage.java
@@ -1,5 +1,31 @@
 package com.fshoot.framepage;
 
-public class ShopPage {
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+
+import com.example.fantasyshooter.R;
+import com.fshoot.main.MyApp;
+
+public class ShopPage implements FramePage {
+	
+	private Activity activity;
+	// 1. GUI
+	public void show(Activity activity, boolean isSaveToRam) {
+		// 1. GUI
+		FrameLayout frame = (FrameLayout) (activity.findViewById(R.id.content_frame));
+		// Remove the old one
+		frame.removeAllViewsInLayout();
+		// Show a new screen
+		LayoutInflater.from(activity).inflate(R.layout.frame_shop, frame,true);
+		
+		if (isSaveToRam) {
+			((MyApp) activity.getApplicationContext()).getScreenList().add(this);
+		}
+		
+		// 2. setup event listener
+		
+		// 3. event start when created
+	}
 
 }
