d641288747385f4d15de3fb1557342aff4cb7fd5
tmrcwwong <tmrcwwong@msn.com>
2014-04-17 17:29:40 +0800

    launcher icon

--- FantasyShooter/src/com/fshoot/framepage/StartPage.java
+++ FantasyShooter/src/com/fshoot/framepage/StartPage.java
@@ -118,7 +118,7 @@ public class StartPage implements FramePage {
 				AlertDialog.Builder myAlertDialog = new AlertDialog.Builder(activity);
 				myAlertDialog.setTitle("About FantasyShooter");
 				myAlertDialog
-						.setMessage("FantasyShooter is devised by Kenneth, Tim, Sing, Near in 2014.");
+						.setMessage("Fantasy Shooter is devised by Kenneth, Tim, Sing, Near in 2014.");
 				DialogInterface.OnClickListener doneClick3 = new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int which) {
 						dialog.dismiss();
